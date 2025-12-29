8822f5a5c347b8920efe16d898286cb8ff681395
durgeshh04 <durgeshchaudhari199@gmail.com>
2025-12-30 02:14:55 +0530

    user model created

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  })
+);
 
 export default app;
